23f50ae6cb7eaabe435f4a34833fb8ab78e24e04
massguunn <rohadigunawan0@gmail.com>
2025-07-28 09:51:04 +0800

    menambahkan notifikasi install pwa

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,16 @@ window.addEventListener("scroll", function () {
 
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Untuk Mobile atau scroll ke atas paling atas
 });
+
+window.addEventListener("load", () => {
+  // Cek apakah user sudah pernah melihat alert sebelumnya
+  if (!localStorage.getItem("pwa_alert_shown")) {
+    setTimeout(() => {
+      alert(
+        "📲 Untuk pengalaman terbaik, tambahkan aplikasi ini ke layar utama Anda."
+      );
+      // Simpan status supaya tidak muncul terus
+      localStorage.setItem("pwa_alert_shown", "yes");
+    }, 1000);
+  }
+});
